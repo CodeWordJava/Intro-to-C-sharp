dbc6d39963f3f14bc4eb9a4ba81dc9cd9d83aaf1
Katie <103640963+CodeWordJava@users.noreply.github.com>
2023-01-19 12:17:22 +0000

    Create firstCalc.cs
    small addition only calculator that handles decimals and converts input from string

---
+++ firstCalc.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+    static void Main(string[] args) 
+  {
+    Console.Write("Pick a number: ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Pick another number: ");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+    
+   
+   Console.WriteLine(num1+num2);
+
+    Console.ReadLine();
+        
+    }
+}
